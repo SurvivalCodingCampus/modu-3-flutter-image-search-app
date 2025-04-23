023827cc69da64ae445ff0d0fb47dd2a3513c4cd
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-04-23 14:04:16 +0900

    fix: DetailViewModel Factory 생성되도록 수정

--- lib/core/di/di_setup.dart
+++ lib/core/di/di_setup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../data/data_source/photo_data_source.dart';
 import '../../data/data_source/pixabay_api.dart';
@@ -33,6 +34,6 @@ final photoDataSourceProvider = Provider<PhotoDataSource>((ref) {
   return PixabayApi();
 });
 
-final detailViewModelProvider = ChangeNotifierProvider<DetailViewModel>((ref) {
+final detailViewModelProvider = ChangeNotifierProvider.autoDispose<DetailViewModel>((ref) {
   return DetailViewModel(ref.read(searchPhotoUseCaseProvider));
 });
